28dc4a81b638216c2567a7a8817422411b8b4aa8
alcedok <kevinalcedo@gmail.com>
2017-06-27 18:13:43 -0400

    restructure code

---
+++ GPU/start.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Terminate if there's an input error
+set -e
+
+# Make sure there are the current number of inputs
+nargs="$#"
+# If there are no arguments display options
+if [ ${nargs} -ne 1 ]; then
+	echo "bash start.sh [option] <command>"
+	echo "error: incorrect number of commands"
+	echo "use -h to see available commands"
+	exit 1
+fi 
+
+# if help (-h) flag is an input
+while getopts ":h" opt; do
+	case ${opt} in 
+		h)
+		echo "commands:"
+		echo "	local"
+		echo "	pull"
+		exit 1
+			;;
+		*) 
+		echo "invalid option: -${OPTARG}"
+		exit 1
+		;;
+	esac
+done
+
+
+# Case statement given command
+args=("$@")
+command=${args[0]}
+
+case ${command} in 
+
+	# Run from local build
+	local)
+	echo "Run from local build"
+	docker run -p 8888:8888  -d -v $PWD/notebooks:/home/tyrell_wellick/notebooks/ nnabla_jupyter:latest
+	;;
+	# Run from cloud build
+	pull)
+	echo "Pull from DockerHub and run"
+	echo "Oh wait...This feature is not ready yet, sorry" 
+	;;
+
+	*) 
+	echo "invalid command. use -h to see available commands "
+	exit 1
+esac
+
+
